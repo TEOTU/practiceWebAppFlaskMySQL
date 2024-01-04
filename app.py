2947ab18fae0ce8803c8da3247c7763dccb36f06
Krasavchik <mhailtnko@gmail.com>
2024-01-04 14:43:31 +0100

    deploying to the cloud

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ About the project: It's just a practice thing for me to learn how websites are b
  BootStrapping
  Dynamic Data using templates
 '''
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -27,6 +27,11 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template('home.html', jobs=JOBS, company_name="Michael's")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
